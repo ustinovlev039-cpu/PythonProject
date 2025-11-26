0aa3063f619827dea54244e7fa479e5431c7c629
recently_was_good <ustinovlev039@gmail.com>
2025-11-26 17:44:33 +1000

    Пример импортов в модуль main

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from src.widget import mask_account_card, get_data
+
+
+if __name__ == "__main__":
+    print(mask_account_card('Счет 73654108430135874305'))
+    print(get_data("2024-03-11T02:26:18.671407"))
+
 
