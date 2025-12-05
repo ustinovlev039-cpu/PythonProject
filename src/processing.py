862384a3a8122e3b1ddf63d9f0baed42b53807e2
recently_was_good <ustinovlev039@gmail.com>
2025-12-05 20:12:44 +1000

    Написание первой рабочей функции

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,25 @@
-def filter_by_state(diction ,state="EXECUTED":  dict) -> dict:
+def filter_by_state(diction: dict ,state="EXECUTED") -> list[dict]:
     """Функция возращающая словарь с соответсвующим ключом state"""
-    pass
+    new_diction = []
+    for dict_item in diction:
+        if dict_item.get("state") == state:
+            new_diction.append(dict_item)
+
+    return new_diction
+
+
 
 
 
 
 def sort_by_date():
     pass
+
+
+diction = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+           {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+           {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+           {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+
+if __name__ == "__main__":
+    print(filter_by_state(diction, state="CANCELED"))
