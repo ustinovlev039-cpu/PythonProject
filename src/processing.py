ed5fd58183ef495dfa3b0bd5b930d536c0fae65d
recently_was_good <ustinovlev039@gmail.com>
2025-12-09 07:51:15 +1000

    Измененные название + докстринги функций в модуле proccessing

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,18 @@
-def filter_by_state(diction: list[dict], state="EXECUTED") -> list[dict]:
-    """Функция возращающая словарь с соответсвующим ключом state"""
-    new_diction = []
-    for dict_item in diction:
+def filter_by_state(list_operation: list[dict], state="EXECUTED") -> list[dict]:
+    """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
+    state соответствует указанному значению."""
+    new_dictionary = []
+    for dict_item in list_operation:
         if dict_item.get("state") == state:
-            new_diction.append(dict_item)
+            new_dictionary.append(dict_item)
 
-    return new_diction
+    return new_dictionary
 
 
-def sort_by_date(date: list[dict], reverse=True) -> list:
-    """Функция сортирующая по датам с возрастающей или по убыванию"""
-    result = sorted(date, key=lambda x: x["date"], reverse=True)
+def sort_by_date(to_do_list: list[dict], reverse=True) -> list:
+    """Функция должна возвращать новый список, отсортированный по дате (date)."""
+    result = sorted(to_do_list, key=lambda x: x["date"], reverse=True)
     return result
 
 
-# Добить, чтобы линтеры не выдавали ошибку + сделать слияние веток + добить README
+
