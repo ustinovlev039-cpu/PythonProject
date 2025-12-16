9f883a04d1c7054a7998a68b7157b5eb90f249f2
recently_was_good <ustinovlev039@gmail.com>
2025-12-16 21:10:46 +1000

    Добавление в пакет параметризацию для тестов каждого модуля + один фикстуру

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,10 @@
+import pytest
 
-
-
+@pytest.fixture
+def test_same_date():
+    return  (
+     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+     {'id': 615064591, 'state': 'CANCELED', 'date': '2019-07-03T18:35:29.512364'}
+     )
 
 
