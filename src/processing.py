c5152fe79d59bdd5c919d2fb885152ec75ef6063
recently_was_good <ustinovlev039@gmail.com>
2025-12-16 21:10:46 +1000

    Добавление в пакет параметризацию для тестов каждого модуля + один фикстуру

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,10 @@
-def filter_by_state(list_operation: list[dict], state="EXECUTED") -> list[dict]:
+def filter_by_state(list_operation: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
     state соответствует указанному значению."""
+    allowed = ("EXECUTED", "CANCELED")
+    if state not in allowed:
+        raise ValueError("Не правильный ввод state")
+
     new_dictionary = []
     for dict_item in list_operation:
         if dict_item.get("state") == state:
@@ -9,9 +13,19 @@ def filter_by_state(list_operation: list[dict], state="EXECUTED") -> list[dict]:
     return new_dictionary
 
 
-def sort_by_date(to_do_list: list[dict], reverse=True) -> list:
+
+
+
+
+def sort_by_date(to_do_list: list[dict], reverse=True) -> list[dict]:
     """Функция должна возвращать новый список, отсортированный по дате (date)."""
-    result = sorted(to_do_list, key=lambda x: x["date"], reverse=True)
+    for item in to_do_list:
+        if "date" not in item:
+            raise KeyError("data")
+        if not isinstance(item["date"], str):
+            raise TypeError("не правильный тип ввода")
+
+    result = sorted(to_do_list, key=lambda x: x["date"], reverse=reverse)
     return result
 
 
--- src/widget.py
+++ src/widget.py
@@ -1,39 +1,37 @@
-# from src.masks import get_mask_account
-# from src.masks import get_mask_card_number
-#
-#
-# def mask_account_card(info_card_check: str | int) -> str:
-#     """Возращает маску карты или счета"""
-#     digits_only = "".join(ch for ch in str(info_card_check).strip() if ch.isdigit())
-#     if len(digits_only) == 16:
-#         mask = get_mask_card_number(digits_only)
-#     elif len(digits_only) == 20:
-#         mask = get_mask_account(digits_only)
-#     else:
-#         return "Не правильный ввод"
-#
-#     return mask
-#
-#
-# if __name__ == "__main__":
-#     print(mask_account_card("Visa Platinum 7000792289606361"))
-#
-#
-# def get_data(date_str: str) -> str:
-#     """Функция принимает дату делает в формате ДД.ММ.ГГГГ"""
-#     only_date = date_str.split("T", 1)[0]
-#     if len(only_date) == 10 or len(only_date) == 8:
-#         if only_date in "-":
-#             year, month, day = only_date.split("-")
-#             return f"{day}.{month}.{year}"
-#         else:
-#             result = only_da
-#
-#
-# if __name__ == "__main__":
-#     print(get_data("2024-03-11T02:26:18.671407"))
+from src.masks import get_mask_account
+from src.masks import get_mask_card_number
 
 
+def mask_account_card(info_card_check: str | int) -> str:
+    """Возращает маску карты или счета"""
+    digits_only = "".join(ch for ch in str(info_card_check).strip() if ch.isdigit())
+    if len(digits_only) == 16:
+        mask = get_mask_card_number(digits_only)
+    elif len(digits_only) == 20:
+        mask = get_mask_account(digits_only)
+    else:
+        return "Не правильный ввод"
 
-result = "20240311"
-print(result.split())
+    return mask
+
+
+if __name__ == "__main__":
+    print(mask_account_card("Visa Platinum 7000792289606361"))
+
+
+def get_data(date_str: str | int) -> str:
+    """Функция принимает дату делает в формате ДД.ММ.ГГГГ"""
+    if len(date_str) > 0:
+        filter_date = str(date_str).strip()
+        only_date = filter_date.split("T", 1)[0]
+        if len(only_date) == 10 or len(only_date) == 8:
+            if "-" in only_date:
+                year, month, day = only_date.split("-")
+                return f"{day}.{month}.{year}"
+            else:
+                return  "Не правильный ввод даты"
+
+
+    return "Не правильный ввод даты"
+
+
