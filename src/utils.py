12ae49596fcd7b5ff4b85e0564b3cdfd94e4b4d1
recently_was_good <ustinovlev039@gmail.com>
2026-01-02 12:28:01 +1000

    Добавление модуля utils and external_api + operations.json + в модуле utils была реализована функция для чтение json

---
+++ src/utils.py
@@ -0,0 +1,13 @@
+import json
+
+def info_transactions_json() -> list[dict]:
+    """ Читает JSON с транзакциями и возвращает их """
+    try:
+        with open("data/operations.json") as f:
+            return json.load(f)
+    except json.JSONDecodeError:
+        return []
+
+
+if __name__ == "__main__":
+    print(info_transactions_json())
