21fa406fc0788e3e23cdd80814b959d07aba2d43
recently_was_good <ustinovlev039@gmail.com>
2026-01-05 16:55:15 +1000

    Добавление тестов на модуль external.api

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,8 @@
 import json
 
+
 def info_transactions_json() -> list[dict]:
-    """ Читает JSON с транзакциями и возвращает их """
+    """Читает JSON с транзакциями и возвращает их"""
     try:
         with open("data/operations.json") as f:
             return json.load(f)
