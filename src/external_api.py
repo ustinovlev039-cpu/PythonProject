e18e88b687864a199d396c202225e3c2d458bf94
recently_was_good <ustinovlev039@gmail.com>
2026-01-05 16:55:15 +1000

    Добавление тестов на модуль external.api

--- src/external_api.py
+++ src/external_api.py
@@ -1,20 +1,19 @@
-from utils import info_transactions_json
-import requests
 import os
+
+import requests
 from dotenv import load_dotenv
+
+from src.utils import info_transactions_json
 
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 
-def filter_transactions(info: str, transactions: list[dict]) -> list[float] | None:
-    """ Функция принимает транзакцию и возращает сумму транзакции """
 
-    paramsa ={
-        "from": "from_cur",
-        "to": "RUB",
-        "amount": "amount"
-    }
+def filter_transactions(info: str, transactions: list[dict]) -> list[float] | None:
+    """Функция принимает транзакцию и возращает сумму транзакции"""
+
+    paramsa = {"from": "from_cur", "to": "RUB", "amount": "amount"}
 
     for dict_ in transactions:
         if dict_.get("id") == int(info):
@@ -28,26 +27,18 @@ def filter_transactions(info: str, transactions: list[dict]) -> list[float] | No
 
     return None
 
-def conversation(info_filter: dict) -> list[dict]:
-    """ Функиция отправки API иноформации и перевода в RUB """
+
+def conversation(info_filter: dict) -> float:
+    """Функиция отправки API иноформации и перевода в RUB"""
     url = "https://api.apilayer.com/exchangerates_data/convert"
-    headers = {
-        "apikey": API_KEY
-    }
+    headers = {"apikey": API_KEY}
     responses = requests.get(url, headers=headers, params=info_filter)
     end_conversation = responses.json()
     return end_conversation["result"]
 
 
-
-
-
 if __name__ == "__main__":
     test = info_transactions_json()
-    test_2 = filter_transactions("939719570", test)
-    #print(filter_transactions("441945886", test))
+    test_2 = filter_transactions("41428829", test)
+    # print(filter_transactions("441945886", test))
     print(conversation(test_2))
-
-
-
-
