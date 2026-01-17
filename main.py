58dcb0a38d162c71a77c640ca91157702851769d
recently_was_good <ustinovlev039@gmail.com>
2026-01-17 20:47:43 +1000

    Почти заверщение доделование самой программы

--- main.py
+++ main.py
@@ -1,33 +1,87 @@
-# from src.masks import get_mask_account
-from src.masks import get_mask_card_number
-
-# from src.processing import filter_by_state
-# from src.processing import sort_by_date
-# from src.widget import get_data
-# from src.widget import mask_account_card
-
-date = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
-
-same_date = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 615064591, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
-]
-
-diction = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-]
+from src.processing import filter_by_state
+from src.processing_bank import process_bank_search
+from src.external_api import filter_utils
+from src.external_api import filter_transactions
+from src.external_api import conversation
+from src.utils_csv_xlsx import info_transactions_xlsx
+from src.utils_csv_xlsx import info_transactions_csv
+from src.utils import info_transactions_json
 
 
-card = 8990922113665229
+def type_file():
+    """Составление разговора пользователя и его ответы"""
+    print("Привет! Добро пожаловать в программу работы с банковскими транзакциями.")
+    print("Выберите необходимый пункт меню:")
+    ans_type = input("""
+    1. Получить информацию о транзакции из JSON-файла.
+    2. Получить информацию о транзакциях из CSV-файла.
+    3. Получить информацию о транзакциях из XLSX-файла.
+    """)
 
-if __name__ == "__main__":
-    # print(filter_by_state(diction))
-    print(get_mask_card_number(card))
+    if ans_type.lower().strip() == "1":
+        print("Для обработки выбран JSON-файла.")
+        return info_transactions_json("data/operations.json")
+    elif ans_type.lower().strip() == "2":
+        print("Для обработки выбран CSV-файла.")
+        return info_transactions_csv("data/transactions.csv")
+    else:
+        print("Для обработки выбран XLSX-файла.")
+        return info_transactions_xlsx("data/transactions.csv")
+
+
+def input_status():
+    while True:
+        status = ("EXECUTED", "CANCELED", "PENDING")
+        ans_finding = input("""
+            Введите статус, по которому необходимо выполнить фильтрацию.
+            Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING.
+            """)
+        if ans_finding.upper().strip() in status:
+            if ans_finding.lower().strip() == "executed":
+                print("Операции отфильтрованы по статусу 'EXECUTED'")
+                filter_by_state(type_file())
+                break
+
+            elif ans_finding.lower().strip() == "canceled":
+                print("Операции отфильтрованы по статусу 'CANCELED'")
+                filter_by_state(type_file())
+                break
+
+            else:
+                print("Операции отфильтрованы по статусу 'PENDING'")
+                filter_by_state(type_file())
+                break
+
+        else:
+            print("""
+            Введите статус, по которому необходимо выполнить фильтрацию.
+            Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING
+            """)
+
+
+    ans_sort = input("Отсортировать операции по дате? Да/Нет")
+
+    ans_sort_type = input("Отсортировать по возрастанию или по убыванию?")
+    if ans_sort_type.lower().strip() == "по возрастанию":
+        ans_sort_type = False
+    else:
+        ans_sort_type = True
+
+    ans_type_transaction = input("Выводить только рублевые транзакции? Да/Нет")
+
+    ans_type_desc_transaction = input("Отфильтровать список транзакций по определенному слову в описании? Да/Нет")
+
+    ans_write_transaction = input("Распечатываю итоговый список транзакций...")
+
+
+
+
+
+
+
+
+starting_progrem()
+
+
+
+
