391ffa16154a7561326f41e38d9174cc2dd65044
recently_was_good <ustinovlev039@gmail.com>
2026-01-19 14:25:30 +1000

    Добивочка

--- main.py
+++ main.py
@@ -1,73 +1,62 @@
-from src.processing import filter_by_state
-from src.processing import sort_by_date
+from src.processing import filter_by_state, sort_by_date
 from src.processing_bank import process_bank_search
+from src.utils_csv_xlsx import info_transactions_xlsx, info_transactions_csv
 from src.utils import info_transactions_json
-from src.utils_csv_xlsx import info_transactions_csv
-from src.utils_csv_xlsx import info_transactions_xlsx
-from src.widget import get_data
-from src.widget import mask_account_card
+from src.widget import mask_account_card, get_data
 
 
 def type_file() -> list[dict]:
-    """Составление разговора пользователя и его ответы о формате"""
+    """Диалог о выборе формата """
     print("Привет! Добро пожаловать в программу работы с банковскими транзакциями.")
     print("Выберите необходимый пункт меню:")
 
     while True:
         ans_type = input(
             """
-        1. Получить информацию о транзакции из JSON-файла.
-        2. Получить информацию о транзакциях из CSV-файла.
-        3. Получить информацию о транзакциях из XLSX-файла.
-        """
+1. Получить информацию о транзакции из JSON-файла.
+2. Получить информацию о транзакциях из CSV-файла.
+3. Получить информацию о транзакциях из XLSX-файла.
+"""
         ).strip()
 
-        if ans_type in ("1", "2", "3"):
-            if ans_type.lower().strip() == "1":
-                print("Для обработки выбран JSON-файла.")
-                return info_transactions_json("data/operations.json")
-            elif ans_type.lower().strip() == "2":
-                print("Для обработки выбран CSV-файла.")
-                return info_transactions_csv("data/transactions.csv")
-            else:
-                print("Для обработки выбран XLSX-файла.")
-                return info_transactions_xlsx("data/transactions_excel.xlsx")
+        if ans_type == "1":
+            print("Для обработки выбран JSON-файл.")
+            return info_transactions_json("data/operations.json")
+        if ans_type == "2":
+            print("Для обработки выбран CSV-файл.")
+            return info_transactions_csv("data/transactions.csv")
+        if ans_type == "3":
+            print("Для обработки выбран XLSX-файл.")
+            return info_transactions_xlsx("data/transactions_excel.xlsx")
 
-        else:
-            print("Не правильный ввод, попробуйте еще раз")
+        print("Не правильный ввод, попробуйте еще раз")
 
 
 def input_status() -> str:
-    """ "Составление разговора пользователя и его ответы о фильтрации"""
+    """Запрашивает статус для фильтрации и возвращает его в верхнем регистре."""
+    allowed = ("EXECUTED", "CANCELED", "PENDING")
     while True:
-        status = ("EXECUTED", "CANCELED", "PENDING")
         ans_finding = input(
             """
-            Введите статус, по которому необходимо выполнить фильтрацию.
-            Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING.
-            """
+Введите статус, по которому необходимо выполнить фильтрацию.
+Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING.
+"""
         )
         normalize = ans_finding.upper().strip()
-        if normalize in status:
+        if normalize in allowed:
             print(f"Операции отфильтрованы по статусу '{normalize}'")
             return normalize
 
-        else:
-            print(
-                """
-            Введите статус, по которому необходимо выполнить фильтрацию.
-            Доступные для фильтровки статусы: EXECUTED, CANCELED, PENDING
-            """
-            )
+        print(f"Статус операции '{ans_finding.strip()}' недоступен.")
 
 
-def only_digits(text: str | int) -> str:
-    """Маску для карты"""
+def only_digits(text: str) -> str:
+    """Оставляет в строке только цифры."""
     return "".join(ch for ch in str(text) if ch.isdigit())
 
 
 def currency_code(op: dict) -> str:
-    """Возвращает код валюты транзакции, в разныз форматах"""
+    """Возвращает валюту и поддерживает, все выбранные форматы."""
     if "currency_code" in op:
         return str(op.get("currency_code", ""))
     try:
@@ -77,43 +66,36 @@ def currency_code(op: dict) -> str:
 
 
 def amount(op: dict) -> str:
-    """Возвращает сумму операции, в разных форматах"""
-    if "amount" in op:
+    """Возвращает сумму операции во всех выборных форматах."""
+    if "amount" in op:  # CSV/XLSX
         return str(op.get("amount"))
-    try:
+    try:  # JSON
         return str(op["operationAmount"]["amount"])
     except Exception:
         return ""
 
 
 def main() -> None:
-    """Составление логики программы"""
     files = type_file()
     result_status = input_status()
 
     list_operation = filter_by_state(files, result_status)
 
-    ans_sort = input("Отсортировать операции по дате? Да/Нет")
+    ans_sort = input("Отсортировать операции по дате? Да/Нет\n")
     if ans_sort.lower().strip() == "да":
-        ans_sort_type = input("Отсортировать по возрастанию или по убыванию?")
-
-        if ans_sort_type.lower().strip() == "по возрастанию":
-            reverse_flag = False
-        else:
-            reverse_flag = True
-
+        ans_sort_type = input("Отсортировать по возрастанию или по убыванию?\n")
+        reverse_flag = ans_sort_type.lower().strip() != "по возрастанию"
         list_operation = sort_by_date(list_operation, reverse_flag)
 
     transaction = list_operation
 
-    ans_type_transaction = input("Выводить только рублевые транзакции? Да/Нет")
+    ans_type_transaction = input("Выводить только рублевые транзакции? Да/Нет\n")
     if ans_type_transaction.lower().strip() == "да":
-        transaction = [op for op in list_operation if currency_code(op) == "RUB"]
-    else:
-        transaction = list_operation
+        transaction = [op for op in transaction if currency_code(op) == "RUB"]
 
-    ans_type_desc_transaction = input("Отфильтровать список транзакций по определенному слову в описании? Да/Нет")
-
+    ans_type_desc_transaction = input(
+        "Отфильтровать список транзакций по определенному слову в описании? Да/Нет\n"
+    )
     if ans_type_desc_transaction.lower().strip() == "да":
         words = input("Введите слово для поиска в описании: ")
         transaction = process_bank_search(transaction, words)
